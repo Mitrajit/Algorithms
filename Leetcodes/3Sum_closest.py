00004d9a03b602bd8dc096945e7cb10c3e282033
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-07-27 21:29:28 +0530

    Comments added

--- Leetcodes/3Sum_closest.py
+++ Leetcodes/3Sum_closest.py
@@ -1,10 +1,14 @@
+from typing import List # Not needed for leetcode editor
+# Time complexity O(N^2)
 class Solution:
     def threeSumClosest(self, nums: List[int], target: int) -> int:
         nums.sort()
         ans = float('inf')
         for i in range(len(nums)-2):
+            # To omit repeated numbers
             if i>0 and nums[i-1] == nums[i]:
                 continue
+            # Getting the range of summation that can be generated
             minsum = nums[i] + nums[i+1] + nums[i+2]
             maxsum = nums[i] + nums[-1] + nums[-2]
             if minsum >= target:
@@ -25,10 +29,12 @@ class Solution:
                     return thsum
                 elif thsum < target:
                     left += 1
+                    # To omit repeated numbers
                     while left < len(nums)-1 and nums[left-1] == nums[left]:
                         left += 1
                 else:
                     right -= 1
+                    # To omit repeated numbers
                     while right > i and nums[right+1] == nums[right]:
                         right -= 1
         return ans
